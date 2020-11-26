41aefdc3ac42aedaedece6dd35caaf813d0c6aaf
RhiannonAW123 <rhiannonaimeewilson@gmail.com>
2020-11-26 12:01:12 +0000

    another test pssed

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,7 +1,9 @@
 def fizz_buzz(number):
-    if number == 3:
+    if number == 15:
+        return "fizzbuzz"
+    elif number % 3 == 0:
       return "fizz"
-    elif number == 5:
+    elif number % 5 == 0:
       return "buzz"
     elif number == 15:
       return "fizzbuzz"
--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -6,12 +6,17 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_fizz_buzz__3_returns_fizz(self):
         self.assertEqual("fizz", fizz_buzz(3))
+    
+    def test_fizz_buzz__6_returns_fizz(self):
+        self.assertEqual("fizz", fizz_buzz(6))
 
-    def test_fizz_buzz__5_returns_buzz(self):
-        self.assertEqual("buzz", fizz_buzz(5))
+    def test_fizz_buzz__10_returns_buzz(self):
+        self.assertEqual("buzz", fizz_buzz(10))
 
     def test_fizz_buzz__15_returns_fizzbuzz(self):
         self.assertEqual("fizzbuzz", fizz_buzz(15))
 
     def test_fizz_buzz__4_returns_4_as_a_str(self):
         self.assertEqual("4", fizz_buzz(4))
+
+    
